a55af66c37ca5a9d0dffcec3f1dd4152b5bd2126
Nhất Thanh Trần Hồ <30461021+nhatthanh1998@users.noreply.github.com>
2024-10-24 10:18:43 +0700

    feat: add fetchPokemon function

--- index.ts
+++ index.ts
@@ -1,6 +1,17 @@
 import { BunRuntime } from "@effect/platform-bun"
-import { Effect } from "effect"
+import { Effect, Either } from "effect"
+import { PokemonSchema } from "./pokemon.js"
+import { Schema } from "@effect/schema"
 
-const main = Effect.logInfo("Hello Effect Coding Challenge")
-
-BunRuntime.runMain(main)
+const fetchPokemon = (pokemonName: string) => Effect.tryPromise({
+    try: async () => {
+        console.log(`Process: ${pokemonName}`)
+        const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+        const validateResult = Schema.decodeUnknownEither(PokemonSchema)(await data.json())
+        if (Either.isRight(validateResult)) {
+            return Effect.succeed(validateResult.right)
+        }
+        return Effect.fail(new Error(validateResult.left.message))
+    },
+    catch: () => new Error('fetch fail'),
+})
